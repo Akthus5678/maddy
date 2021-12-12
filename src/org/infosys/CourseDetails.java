4708cdc9c2977a152ea1a9f13e90c7be2a1cfa27
Akthus5678 <ahamadakthus123@gmail.com>
2021-12-12 16:05:20 +0530

    hello

---
+++ src/org/infosys/CourseDetails.java
@@ -0,0 +1,40 @@
+package org.infosys;
+
+public class CourseDetails {
+	public static void main(String[] args) {
+		byte empAge = 27;
+		System.out.println(empAge);
+		System.out.println("employee age is"+empAge);
+		
+	    short empId = 3245;
+	    System.out.println("Employee id is"+ empId);
+	    
+	    int empPhone = 770886661;
+	    System.out.println("Employee phone is"+ empPhone);
+	    
+	    long empPhoneNo = 6381408885l;
+	    System.out.println("Employee phone number is"+ empPhoneNo);
+        
+	    float empSalary = 45.20f;
+	    System.out.println("Employee salary is "+empSalary);
+	    float x = 45/9f;
+	    System.out.println(x);
+	    
+	    double empAnswer = 3224533.34345335;
+	    System.out.println("Employee answer is"+empAnswer);
+	    double g = 45/9d;
+	    System.out.println(g);
+	    
+	    char empGender = 'M';
+	    System.out.println("Employee gender is "+empGender);
+	    
+	    String empEmail = "knowledge245@gmail.com";
+	    System.out.println("Employee email is"+ empEmail);
+	    
+	    boolean empStatus = true;
+	    System.out.println("Employee status is "+ empStatus);
+	    
+	    
+	}
+
+}
